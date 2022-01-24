c2f51df5cfcbab917bb1305583796b44e98ee929
Julius B <36518501+silver1bear@users.noreply.github.com>
2022-01-24 16:07:17 +0100

    clean up, set CORS

--- SocketsServer.js
+++ SocketsServer.js
@@ -8,6 +8,8 @@ const app = express();
  */
 app.get("/", (req, res) => {
 
+	res.setHeader("Access-Control-Allow-Origin", "*");
+
 	res.write("Socket-Server\r\n");
 	res.write("usage: /:socketNo?action=[1|0]\r\n");
 
@@ -20,6 +22,12 @@ app.get("/", (req, res) => {
  */
 app.get("/:socketNo", (req, res) => {
 
+	res.setHeader("Access-Control-Allow-Origin", "*");
+
+	//Basic injection prevention
+	if(parseInt(req.params.socketNo) == NaN || parseInt(req.query.action) == NaN)
+		return;
+
 	exec(`/home/pi/SocketControl ${req.params.socketNo} ${req.query.action}`, (i,j,k)=>{return;});
 
 	res.status(200).send(`Turned socket ${req.params.socketNo} ${req.query.action == "1" ? "on" : "off"}\r\n`);
@@ -27,58 +35,3 @@ app.get("/:socketNo", (req, res) => {
 });
 
 app.listen(6969);
-
-// var http = require('http');
-// var url = require('url');
-// const { exec } = require('child_process');
-// const execSync = require("child_process").execSync;
-
-// http.createServer(function (req, res) {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-
-//   var url_parts = url.parse(req.url, true);
-//   var query = url_parts.query;
-  
-//   console.log(query["action"]);
-
-//   //turnon: 	Turn all sockets on
-//   //turonoff: 	Turn all socket off
-//   //on:		Turn specific socket on
-//   //off:	Turn specific socket off
-//   //action:	any other actions (only raspi CPU temp right now)
-  
-//   if(query["action"] == "turnon"){
-// 	res.end("An");
-// 	exec("/home/pi/SocketControl -on", (i,j,k)=>{return;});
-//   } else if(query["action"] == "turnoff"){
-// 	exec("/home/pi/SocketControl -off", (i,j,k)=>{return;});
-//   	res.end("Aus");
-//   } else if(query["action"]=="on"){
-// 	let socketNo = -1;
-// 	try{
-// 		socketNo = parseInt(query["no"])
-// 	} catch(e){
-// 		res.end(e);
-// 	}
-// 	exec("/home/pi/SocketControl " + parseInt(query["no"]) + " 1", (i,j,k)=>{return;});
-// 	res.end("Turned on socket no " + socketNo);
-//   } else if(query["action"]=="off"){
-// 	let socketNo = -1;
-// 	try{
-// 		socketNo = parseInt(query["no"])
-// 	} catch(e){
-// 		res.end(e);
-// 	}
-// 	exec("/home/pi/SocketControl " + parseInt(query["no"]) + " 0", (i,j,k)=>{return;});
-// 	res.end("Turned on socket off " + socketNo);
-//   } else if(query["action"]== "check_temp"){
-//   	var cpuTemp = execSync("vcgencmd measure_temp");
-// 	cpuTemp = cpuTemp.toString();
-// 	cpuTemp = cpuTemp.slice(5);
-// 	cpuTemp = cpuTemp.replace(".", ",");
-// 	cpuTemp = cpuTemp.replace("C", "Grad");
-// 	res.end(cpuTemp);
-//   } else res.end("Fehler");
-
-// }).listen(6969);
--- execTelnetTest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-TELNETANSWER="$(./AsusTelnet.sh)"
-echo "${TELNETANSWER}"
